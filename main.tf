c2e783323e002c9631deabfafaa8e3c48767e372
Joe Hosteny <jhosteny@gmail.com>
2023-07-03 17:34:52 -0400

    feat: upgrade for latest Terraform version and CloudPosse tooling

--- main.tf
+++ main.tf
@@ -1,27 +1,17 @@
-module "label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.3"
-  namespace  = var.namespace
-  name       = var.name
-  stage      = var.stage
-  delimiter  = var.delimiter
-  attributes = var.attributes
-  tags       = var.tags
-}
-
 locals {
-  enabled      = var.enabled == "true" ? true : false
+  enabled      = module.this.enabled
   require_mfa  = var.require_mfa == "true" ? true : false
-  role_arns    = ["${values(var.role_arns)}"]
-  role_aliases = ["${keys(var.role_arns)}"]
+  role_arns    = values(var.role_arns)
+  role_aliases = keys(var.role_arns)
 }
 
 resource "null_resource" "role" {
   count = length(values(var.role_arns))
 
   triggers = {
-    account_id = "${element(split(":", element(local.role_arns, count.index)), 4)}"
-    role_name  = "${element(split("/", element(split(":", element(local.role_arns, count.index)), 5)), 1)}"
-    alias      = "${element(local.role_aliases, count.index)}"
+    account_id = element(split(":", element(local.role_arns, count.index)), 4)
+    role_name  = element(split("/", element(split(":", element(local.role_arns, count.index)), 5)), 1)
+    alias      = element(local.role_aliases, count.index)
   }
 
   lifecycle {
@@ -32,14 +22,14 @@ resource "null_resource" "role" {
 # https://www.terraform.io/docs/providers/aws/r/iam_group.html
 resource "aws_iam_group" "default" {
   count = local.enabled ? 1 : 0
-  name  = module.label.id
+  name  = module.this.id
 }
 
 # https://www.terraform.io/docs/providers/aws/r/iam_user_group_membership.html
 resource "aws_iam_user_group_membership" "default" {
   count  = local.enabled && length(var.user_names) > 0 ? length(var.user_names) : 0
   user   = element(var.user_names, count.index)
-  groups = ["${join("", aws_iam_group.default.*.id)}"]
+  groups = [join("", aws_iam_group.default.*.id)]
 }
 
 # https://www.terraform.io/docs/providers/aws/d/iam_policy_document.html
@@ -51,7 +41,7 @@ data "aws_iam_policy_document" "with_mfa" {
       "sts:AssumeRole",
     ]
 
-    resources = ["${local.role_arns}"]
+    resources = local.role_arns
 
     condition {
       test     = "Bool"
@@ -65,9 +55,9 @@ data "aws_iam_policy_document" "with_mfa" {
 
 resource "aws_iam_group_policy" "with_mfa" {
   count  = local.enabled && local.require_mfa ? 1 : 0
-  name   = module.label.id
+  name   = module.this.id
   group  = join("", aws_iam_group.default.*.id)
-  policy = data.aws_iam_policy_document.with_mfa.json
+  policy = join("", data.aws_iam_policy_document.with_mfa.*.json)
 }
 
 data "aws_iam_policy_document" "without_mfa" {
@@ -78,7 +68,7 @@ data "aws_iam_policy_document" "without_mfa" {
       "sts:AssumeRole",
     ]
 
-    resources = ["${local.role_arns}"]
+    resources = local.role_arns
 
     effect = "Allow"
   }
@@ -86,7 +76,7 @@ data "aws_iam_policy_document" "without_mfa" {
 
 resource "aws_iam_group_policy" "without_mfa" {
   count  = local.enabled && local.require_mfa == false ? 1 : 0
-  name   = module.label.id
+  name   = module.this.id
   group  = join("", aws_iam_group.default.*.id)
-  policy = data.aws_iam_policy_document.without_mfa.json
+  policy = join("", data.aws_iam_policy_document.without_mfa.*.json)
 }
--- variables.tf
+++ variables.tf
@@ -15,47 +15,8 @@ variable "require_mfa" {
   description = "Require the users to have MFA enabled"
 }
 
-variable "enabled" {
-  type        = string
-  description = "Whether to create these resources"
-  default     = "true"
-}
-
-variable "namespace" {
-  type        = string
-  description = "Namespace (e.g. `cp` or `cloudposse`)"
-}
-
-variable "stage" {
-  type        = string
-  description = "Stage (e.g. `prod`, `dev`, `staging`, `infra`)"
-}
-
-variable "name" {
-  type        = string
-  description = "Name  (e.g. `app` or `cluster`)"
-}
-
 variable "switchrole_url_template" {
   type        = string
   description = "URL template for the IAM console to switch to the roles"
   default     = "https://signin.aws.amazon.com/switchrole?account=%s&roleName=%s&displayName=%s"
-}
-
-variable "delimiter" {
-  type        = string
-  default     = "-"
-  description = "Delimiter to be used between `namespace`, `stage`, `name`, and `attributes`"
-}
-
-variable "attributes" {
-  type        = list(string)
-  default     = []
-  description = "Additional attributes (e.g. `1`)"
-}
-
-variable "tags" {
-  type        = map(string)
-  default     = {}
-  description = "Additional tags (e.g. map(`BusinessUnit`,`XYZ`)"
 }
